48bd44547a44a06bf4c5a695d5aaa07ef9683906
Maksim <makushimo@mail.ru>
2020-09-06 20:27:07 +0300

    lombok

--- src/main/java/guru/springframework/api/domain/UserData.java
+++ src/main/java/guru/springframework/api/domain/UserData.java
@@ -1,19 +1,17 @@
 package guru.springframework.api.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
 /**
  * Created by jt on 9/21/17.
  */
+@Getter
+@Setter
 public class UserData {
 
     List<User> data;
 
-    public List<User> getData() {
-        return data;
-    }
-
-    public void setData(List<User> data) {
-        this.data = data;
-    }
 }
